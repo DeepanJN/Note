ba44cad2a24d7f7990f512943f595b4f10611328
deepaj-6611 <deepajothinathan.p@zohocorp.com>
2023-11-21 00:11:55 +0530

    Room issue solved with suspend

--- app/src/main/java/com/deejayen/note/database/CustomModels.kt
+++ app/src/main/java/com/deejayen/note/database/CustomModels.kt
@@ -5,11 +5,10 @@ import androidx.room.Relation
 import com.deejayen.note.database.entity.Note
 import com.deejayen.note.database.entity.NoteContent
 
-class CustomModels {
 
-    data class NoteWithContent(
-        @Embedded val note: Note,
-        @Relation(parentColumn = "noteId", entityColumn = "noteId")
-        val noteDetails: List<NoteContent>,
-    )
-}
+data class NoteWithContent(
+    @Embedded val note: Note,
+    @Relation(parentColumn = "noteId", entityColumn = "noteId")
+    val noteDetails: List<NoteContent>,
+)
+
--- app/src/main/java/com/deejayen/note/database/dao/NoteDao.kt
+++ app/src/main/java/com/deejayen/note/database/dao/NoteDao.kt
@@ -1,9 +1,30 @@
 package com.deejayen.note.database.dao
 
 import androidx.room.*
+import com.deejayen.note.database.NoteWithContent
+import com.deejayen.note.database.entity.Note
+import com.deejayen.note.database.entity.NoteContent
 
 @Dao
 interface NoteDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note): Long
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNoteContent(noteContent: NoteContent): Long
+
+    @Transaction
+    @Query("SELECT * FROM Note INNER JOIN NoteContent ON Note.noteId = NoteContent.noteId")
+    suspend fun getAllNotesWithContent(): List<NoteWithContent>
+
+    @Transaction
+    @Query("SELECT * FROM Note INNER JOIN NoteContent ON Note.noteId = NoteContent.noteId WHERE Note.noteId = :noteId")
+    suspend fun getNoteWithContentById(noteId: Int): NoteWithContent
+
+    @Update
+    suspend fun updateNoteContent(noteContent: NoteContent)
+
+    @Delete
+    suspend fun deleteNoteContent(noteContent: NoteContent)
 }
