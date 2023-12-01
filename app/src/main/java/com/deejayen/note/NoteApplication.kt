53bd04419999349abcdd94a70d575b1ecb45addf
deepaj-6611 <deepajothinathan.p@zohocorp.com>
2023-12-01 20:26:23 +0530

    Dynamic color icons and themes added

--- app/src/main/java/com/deejayen/note/NoteApplication.kt
+++ app/src/main/java/com/deejayen/note/NoteApplication.kt
@@ -1,10 +1,16 @@
 package com.deejayen.note
 
 import com.deejayen.note.di.DaggerAppComponent
+import com.google.android.material.color.DynamicColors
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class NoteApplication : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 
     override fun applicationInjector(): AndroidInjector<NoteApplication>? {
         return DaggerAppComponent.builder().create(this)
